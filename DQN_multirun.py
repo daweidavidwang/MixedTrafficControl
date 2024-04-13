ce1b1663c6aec7bd8ff5f4ee5475b8222ad081c1
dawei <dawei@connect.hku.hk>
2024-04-13 08:39:44 +0800

    add multi run DQN

---
+++ DQN_multirun.py
@@ -0,0 +1,137 @@
+
+import argparse
+import os
+import random
+
+import ray
+from ray import air, tune
+from ray.rllib.algorithms.dqn import DQNConfig, DQNTorchPolicy
+from Env import Env
+from ray.rllib.examples.models.shared_weights_model import (
+    SharedWeightsModel1,
+    SharedWeightsModel2,
+    TF2SharedWeightsModel,
+    TorchSharedWeightsModel,
+)
+from ray.rllib.models import ModelCatalog
+from ray.rllib.policy.policy import PolicySpec
+from ray.rllib.utils.framework import try_import_tf
+from ray.rllib.utils.test_utils import check_learning_achieved
+from core.custom_logger import CustomLoggerCallback
+
+
+tf1, tf, tfv = try_import_tf()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--num-cpus", type=int, default=0)
+parser.add_argument(
+    "--framework",
+    choices=["tf", "tf2", "torch"],
+    default="torch",
+    help="The DL framework specifier.",
+)
+parser.add_argument(
+    "--as-test",
+    action="store_true",
+    help="Whether this script should be run as a test: --stop-reward must "
+    "be achieved within --stop-timesteps AND --stop-iters.",
+)
+parser.add_argument(
+    "--stop-iters", type=int, default=50, help="Number of iterations to train."
+)
+parser.add_argument(
+    "--stop-timesteps", type=int, default=10000000, help="Number of timesteps to train."
+)
+parser.add_argument(
+    "--stop-reward", type=float, default=150.0, help="Reward at which we stop training."
+)
+
+if __name__ == "__main__":
+    rv_rate_list = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    
+    args = parser.parse_args()
+    ray.init(num_gpus=1, num_cpus=32)
+    for rv_rate in rv_rate_list:
+
+        dummy_env = Env({
+                "junction_list":['229','499','332','334','140'],
+                "spawn_rl_prob":{},
+                "probablity_RL":args.rv_rate,
+                "cfg":'real_data/osm.sumocfg',
+                "render":False,
+                "map_xml":'real_data/CSeditClean_1.net_threelegs.xml',
+                "max_episode_steps":1000,
+                "conflict_mechanism":'flexible',
+                "traffic_light_program":{
+                    "disable_state":'G',
+                    "disable_light_start":0
+                }
+            })
+        obs_space = dummy_env.observation_space
+        act_space = dummy_env.action_space
+        dummy_env.close()
+        policy = {
+            "shared_policy": (
+                DQNTorchPolicy,
+                obs_space,
+                act_space,
+                None
+            )}
+        policy_mapping_fn = lambda agent_id, episode, worker, **kwargs: "shared_policy"
+                
+        config = (
+            DQNConfig()
+            .environment(Env, env_config={
+                "junction_list":['229','499','332','334','140'],
+                "spawn_rl_prob":{},
+                "probablity_RL":args.rv_rate,
+                "cfg":'real_data/osm.sumocfg',
+                "render":False,
+                "map_xml":'real_data/CSeditClean_1.net_threelegs.xml',
+                # "rl_prob_range": [i*0.1 for i in range(5, 10)], # change RV penetration rate when reset
+                "max_episode_steps":1000,
+                "conflict_mechanism":'flexible',
+                "traffic_light_program":{
+                    "disable_state":'G',
+                    "disable_light_start":0 
+                }
+            }, 
+            auto_wrap_old_gym_envs=False)
+            .framework(args.framework)
+            .training(
+                num_atoms=51,
+                noisy=False,
+                hiddens= [512, 512, 512],
+                dueling=True,
+                double_q=True,
+                replay_buffer_config={
+                    'type':'MultiAgentPrioritizedReplayBuffer',
+                    'prioritized_replay_alpha':0.5,
+                    'capacity':50000,
+                }
+            )
+            .rollouts(num_rollout_workers=30, rollout_fragment_length="auto")
+            .multi_agent(policies=policy, policy_mapping_fn=policy_mapping_fn)
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.ass 'ray.rllib.policy.policy_template.DQNTorchPolicy'> for PolicyID=shared_policy
+            .resources(num_gpus=1, num_cpus_per_worker=1)
+            .callbacks(CustomLoggerCallback)
+        )
+
+        stop = {
+            "training_iteration": args.stop_iters,
+        }
+
+        results = tune.Tuner(
+            "DQN",
+            param_space=config.to_dict(),
+            run_config=air.RunConfig(name='DQN_RV'+str(rv_rate), stop=stop, verbose=3, log_to_file=True, 
+            checkpoint_config=air.CheckpointConfig(
+                num_to_keep = 40,
+                checkpoint_frequency = 10
+            )),
+        ).fit()
+
+        if args.as_test:
+            check_learning_achieved(results, args.stop_reward)
+    ray.shutdown()
